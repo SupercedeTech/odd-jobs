c485fb58cbbf32d05dcb6a814a6f72792a07230a
Isaac van Bakel <isaac@supercede.com>
2022-02-02 17:54:37 +0000

    Visually collapse large job payloads in UI
    Very large job payloads can clog up the UI because they are displayed
    completely everywhere they appear. This steals some code from job errors
    (which are always collapsed) to collapse "large" payloads and make the
    user expand them to view them instead.
    
    Make payloads display better - don't try to inline them, and break on
    spaces in the payload so that they are formatted a bit more nicely -
    since large payloads are now collapsed by default, there's no need to
    try to display it all on one line.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,11 @@
+function togglePayload(e) {
+  var $e = $(e).closest('.job-payload');
+  $e.toggleClass('expanded').toggleClass('collapsed');
+  $e.find('.badge.payload-expand').toggleClass('d-none');
+  $e.find('.badge.payload-collapse').toggleClass('d-none');
+  return false;
+}
+
 function toggleError(e) {
   var $e = $(e).closest('.job-error');
   $e.toggleClass('expanded').toggleClass('collapsed');
